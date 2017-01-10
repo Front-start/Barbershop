6c33eb7b9b1ade282c3b92514eae6cb62ada7e49
Front-start <borodarox@gmail.com>
2017-01-10 22:43:52 +0300

    Первый сверстанный макет

---
+++ assets/js/functions.js
@@ -0,0 +1,41 @@
+(function ($) {
+    'use strict';
+
+    $(document).ready(function () {
+
+        $(".close-modal").click(function () {
+            if (event.preventDefault) {
+                event.preventDefault();
+            } else {
+                event.returnValue = false;
+            }
+            $(".container-modal").fadeOut(500);
+            $('body').css('padding-right', '0');
+            $('body').css('overflow', 'visible');
+        });
+
+        $(".modal-show").click(function () {
+            if (event.preventDefault) {
+                event.preventDefault();
+            } else {
+                event.returnValue = false;
+            }
+            $(".login-container").fadeIn(500);
+            $('body').css('overflow', 'hidden');
+
+        });
+
+        $(".map-show").click(function () {
+            if (event.preventDefault) {
+                event.preventDefault();
+            } else {
+                event.returnValue = false;
+            }
+            $(".map-container").fadeIn(500);
+            $('body').css('overflow', 'hidden');
+
+        });
+
+    }); //end ready
+
+}(jQuery));
